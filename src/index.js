42425723132ad6edf00642c795368d021e215e13
VitaliiKarpovets <k0rpsik@gmail.com>
2021-10-14 01:56:10 +0300

    final

--- src/index.js
+++ src/index.js
@@ -3,4 +3,38 @@ import cardFoodTpl from './templates/cardFoodTemplate.hbs'
 import menu from './menu.json';
 
 
-document.querySelector('.js-menu').insertAdjacentHTML('beforeend', cardFoodTpl(menu))
+document.querySelector('.js-menu').insertAdjacentHTML('beforeend', cardFoodTpl(menu));
+
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+const themeSwitch = document.querySelector('#theme-switch-toggle');
+const bodyTheme = document.querySelector('body');
+
+function themeDefault() {
+    const savedTheme = localStorage.getItem('theme');
+    if (!savedTheme) {
+        bodyTheme.classList.add(Theme.LIGHT)
+    } else {
+        bodyTheme.classList.add(savedTheme)
+    }
+    if (bodyTheme.classList.contains(Theme.DARK)) {
+        themeSwitch.checked = true;
+    }
+}
+
+themeDefault();
+
+themeSwitch.addEventListener('change', () => {
+    bodyTheme.classList.toggle(Theme.DARK)
+
+    if (bodyTheme.classList.contains(Theme.DARK)) {
+        localStorage.setItem('theme', Theme.DARK)
+    } else {
+       localStorage.setItem('theme', Theme.LIGHT) 
+    }
+})
+
