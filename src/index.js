5049331a5ad9061fa9fcd15f591fe6797c2ec61a
VitaliiKarpovets <k0rpsik@gmail.com>
2021-10-13 22:29:35 +0300

    promo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+import cardFoodTpl from './templates/cardFoodTemplate.hbs'
+import menu from './menu.json';
+
+
+document.querySelector('.js-menu').insertAdjacentHTML('beforeend', cardFoodTpl(menu))
